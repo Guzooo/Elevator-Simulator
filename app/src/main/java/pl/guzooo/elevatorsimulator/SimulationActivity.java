f72c02eeeddf762c743dbc8df138dedb1f659c11
Guzooo <guzooo-guzooo@o2.pl>
2023-05-10 21:00:01 +0200

    Initial commit

---
+++ app/src/main/java/pl/guzooo/elevatorsimulator/SimulationActivity.java
@@ -0,0 +1,14 @@
+package pl.guzooo.elevatorsimulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimulationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symulation);
+    }
+}
